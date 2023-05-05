fe875facf3d83b35279d0cd8f21d85d5fa5979ec
Mitali Shah <mitalishaleenshah@gmail.com>
2023-05-05 08:27:35 -0500

    initial code to add firebase to the webapp and added basic event handling setup to log out the console when something is added to the text area

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://wearethechampions-9c220-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorsementsInDB = ref(database, "endorsements");
+
+
 let textAreaforEndorsement = document.querySelector("#endorsement-text");
 let publishBtn = document.querySelector("#publish-btn");
+
+publishBtn.addEventListener("click", function() {
+    let valueFromTextArea = textAreaforEndorsement.value;
+    console.log(valueFromTextArea);
+})
